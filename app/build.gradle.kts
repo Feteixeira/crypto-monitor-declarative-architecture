d637c147f8af17a2ea6ff58dfe01f65a50b728c8
unknown <felipe.maringoli@gmail.com>
2026-04-27 15:35:30 -0300

    build: update AndroidManifest, app build.gradle and libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,10 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // material
+    implementation(libs.androidx.compose.material.icons.extended)
+
+    // Retrofit
+    implementation(libs.squareup.retrofit)
+    implementation(libs.squareup.retrofit.gson)
 }
